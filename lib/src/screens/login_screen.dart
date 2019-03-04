be521ec0852ef1b7fda4c7b7ec50619c3cf0fa2d
wanfintara <wanfintara@gmail.com>
2019-03-04 22:30:55 +0700

    Login screen with email address field

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -9,7 +9,37 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: EdgeInsets.all(20),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField();
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Submit'),
+      onPressed: () {},
     );
   }
 }
