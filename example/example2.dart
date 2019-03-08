42320e915c67d08afe2f9527f00befd419cb0333
wanfintara <wanfintara@gmail.com>
2019-03-08 23:50:36 +0700

    Example 2

---
+++ example/example2.dart
@@ -0,0 +1,20 @@
+/// * try at dartpad online
+
+/*
+import 'dart:html';
+
+void main() {
+  final ButtonElement button = querySelector('button');
+
+  button.onClick
+      .timeout(Duration(seconds: 1),
+          onTimeout: (sink) => sink.addError('You Lost!!!!'))
+      .listen(
+        (event) {},
+        onError: (err) => print(err),
+      );
+}
+*/
+
+// HTML code
+// <button>Click me</button>
