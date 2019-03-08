bca4b2aa1c653d8c9e97b3d8d33d061691bb4b72
wanfintara <wanfintara@gmail.com>
2019-03-09 00:27:16 +0700

    More streams examples

--- example/example2.dart
+++ example/example2.dart
@@ -18,3 +18,65 @@ void main() {
 
 // HTML code
 // <button>Click me</button>
+
+
+/*
+import 'dart:html';
+
+void main() {
+  final ButtonElement button = querySelector('button');
+  final InputElement input = querySelector('input');
+  
+  button.onClick
+    .take(4)
+    .where((event) => input.value == 'banana')
+    .listen(
+  		(event) => print('You got it'),
+    	onDone: () => print('Nope, wrong guesses.'),
+  	);
+  
+}
+
+<h4>Guess the word!!!</h4>
+<input/>
+<button>Guess!</button>
+
+*/
+
+/*
+
+import 'dart:html';
+import 'dart:async';
+
+void main () {
+  final InputElement input = querySelector('input');
+  final DivElement div = querySelector('div');
+  
+  final validator = StreamTransformer.fromHandlers(
+  	handleData: (inputValue, sink) {
+      if (inputValue.contains('@')) {
+        sink.add(inputValue);
+      } else {
+        sink.addError('Enter a valid email');
+      }
+    }
+  );
+  
+  input.onInput
+    .map((dynamic event) => event.target.value)
+  	.transform(validator)
+    .listen(
+  		(inputValue) {
+       	print('that email looks valid');
+        div.innerHtml = '';
+      },
+    	onError: (err) => div.innerHtml = err,
+  	);
+}
+
+
+<h4>Enter your email</h4>
+<input/>
+<div style="color: red;"></div>
+
+*/
